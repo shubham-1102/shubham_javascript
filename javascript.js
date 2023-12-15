6a7137b7e2b878813777f01c1a5ecc6ebaeb079e
shubham-1102 <65309865+shubham-1102@users.noreply.github.com>
2023-12-15 17:37:08 +0530

    Message function created
    Message function created

--- javascript.js
+++ javascript.js
@@ -1,26 +1,22 @@
 function Validateinputs() {
     let flag = true;
 
-    
-
     var elements = document.getElementsByName("inlineRadioOptions");
-
     var element = document.getElementsByName("callput");
 
     var strikeprice = document.getElementById("strikePrice").value;
     var buyprice = document.getElementById("buyAbove").value;
     var target = document.getElementById("target1").value;
     var targetmax = document.getElementById("targetmax").value;
+   
     
     for (var i = 0, l = elements.length; i < l; i++)
     {
        // alert(elements[i]);
         if (elements[i].checked)
         {
-            
             // flag = elements[i].value;
             flag = true;
-           
             break;
            
         }else{
@@ -29,11 +25,11 @@ function Validateinputs() {
     }
 
     if(flag==false){
-        alert("Please Enter all the Fields");
+        alert("Please Enter all the Fields qwqwq");
     }
 
-   
-    for (var j = 0, l = element.length; j < l; j++)
+  
+    for (var j = 0, p = element.length; j < p; j++)
     {
         if (element[j].checked)
         {
@@ -41,22 +37,80 @@ function Validateinputs() {
             flag = true;
             break;
         }else{
-            
+            flag = false
         }
     }
 
-    if(strikeprice || buyprice || target || targetmax == ""){
+    
+    console.log(flag);
+    if(strikeprice.trim() || buyprice.trim() || target.trim() || targetmax.trim() == ""){
         flag = false;
     }
 
-    
-
-
+    console.log(flag);
     if(flag==false ){
         alert("Please Enter all the Fields");
+    }else{
+        TextGenration();
     }
 
+   
+    
   }
+
+function TextGenration(){
+    var tradeelements = document.getElementsByName("inlineRadioOptions");
+    var callelements = document.getElementsByName("callput");
+
+    var tradeoption = "";
+    var calloption = "";
+    var strikeprice = document.getElementById("strikePrice").value;
+    var buyprice = document.getElementById("buyAbove").value;
+    var target = parseInt(document.getElementById("target1").value);
+    var targetmax = parseInt(document.getElementById("targetmax").value);
+    
+
+    var Message = "";
+    var targetMessage = target.toString(10);
+
+
+    
+
+    for (var i = 0, l = tradeelements.length; i < l; i++)
+    {
+      
+        if (tradeelements[i].checked)
+        {
+            tradeoption = tradeelements[i].value;
+            break;
+        }
+    }
+
+    for (var j = 0, l = callelements.length; j < l; j++)
+    {
+        if (callelements[j].checked)
+        {
+            calloption = callelements[j].value;
+            break;
+        }
+    }
+    
+    for (var j = target; j <= targetmax; j=j+5)
+    {
+        targetMessage = targetMessage+"-"+ j.toString(10);
+    }
+    
+
+    Message = "<b>TODAY'S TRADE</b><br>"+
+                tradeoption.toUpperCase()+" "+strikeprice+" "+calloption.toUpperCase()+"<br>"+
+                "BUY ABOVE "+buyprice+"<br>"+
+                "TARGET <br>"+
+                targetMessage+"++";
+
+     document.getElementById("demo").innerHTML = Message;
+
+
+}
 
 function MessageSend(){
 
